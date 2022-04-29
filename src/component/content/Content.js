b27d7726ff3b7b6357fd8f8b707e0d0232d5ff02
jeanpiffaut <piffaut.jean@gmail.com>
2022-04-28 22:33:01 -0400

    navbar without buttons

---
+++ src/component/content/Content.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+export class Content extends Component {
+    render() {
+        return (
+            <div>
+                a
+            </div>
+        );
+    }
+}
